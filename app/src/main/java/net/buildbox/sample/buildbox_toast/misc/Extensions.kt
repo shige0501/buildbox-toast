68a08be1212b2f41205a0fe5fcdf045dd57282ae
Koji Shigemura <shige0501@gmail.com>
2018-01-20 00:15:38 +0900

    文字列リソースを指定する場合のサンプルも追加

--- app/src/main/java/net/buildbox/sample/buildbox_toast/misc/Extensions.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_toast/misc/Extensions.kt
@@ -1,7 +1,11 @@
 package net.buildbox.sample.buildbox_toast.misc
 
 import android.content.Context
+import android.support.annotation.StringRes
 import android.widget.Toast
 
 fun Context.showToast(text: CharSequence) =
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+fun Context.showToast(@StringRes res: Int) =
+    Toast.makeText(this, res, Toast.LENGTH_SHORT).show()
